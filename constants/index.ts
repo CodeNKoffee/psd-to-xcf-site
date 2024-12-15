7963a81a867a65fe6e000ddd250341cd4104da60
Hatem Soliman <hatemthedev@gmail.com>
2024-12-15 14:44:58 +0200

    Added footerlinks for other hustles

--- constants/index.ts
+++ constants/index.ts
@@ -40,31 +40,6 @@ export const footerLinks = [
   //   ],
   // },
   {
-    title: "Connect",
-    links: [
-      // { 
-      //   name: "Purchase", 
-      //   href: "#start-packshipping",
-      // },
-      { 
-        name: "LinkedIn", 
-        href: "https://linkedin.com/in/h4temsoliman",
-      },
-      { 
-        name: "GitHub", 
-        href: "https://github.com/CodeNKoffee",
-      },
-      { 
-        name: "X", 
-        href: "https://x.com/h4temsoliman",
-      },
-      { 
-        name: "Portfolio", 
-        href: "https://hatemsolima.dev",
-      },
-    ],
-  },
-  {
     title: "More",
     links: [
       // { 
@@ -81,6 +56,43 @@ export const footerLinks = [
       },
     ],
   },
+  {
+    title: "By the Maker of PackShip",
+    links: [
+      { 
+        name: "React Framify", 
+        href: "https://react-framify.hatemsoliman.dev/",
+      },
+      { 
+        name: "GitHub Edge", 
+        href: "https://github-edge.hatemsoliman.dev/",
+      },
+      { 
+        name: "Memento", 
+        href: "https://memento-game.firebaseapp.com/",
+      },
+      { 
+        name: "LayerLeap", 
+        href: "https://layerleap.hatemsoliman.dev/",
+      },
+      { 
+        name: "Quantum Fly", 
+        href: "https://quantumfly.hatemsoliman.dev/",
+      },
+      { 
+        name: "Polar Paradise", 
+        href: "https://polarparadise.hatemsoliman.dev/",
+      },
+      { 
+        name: "Splanda Media", 
+        href: "https://splandamedia.com/",
+      },
+      { 
+        name: "QMaster", 
+        href: "https://linkedin.com/company/qmasterapp/",
+      },
+    ],
+  }
 ];
 
 export const socials = [
